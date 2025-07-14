5705597ac4890009c1fe20ee4ca2003d250dda1b
GGabilondo <ggabilondo.1@gmail.com>
2025-07-14 20:15:02 +0200

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero = () => {
           alt="Luxury car detailing" 
           className="w-full h-full object-cover"
         />
-        <div className="absolute inset-0 bg-gradient-to-b from-black/40 via-transparent to-black/60"></div>
+        <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/20 to-black/70"></div>
       </div>
 
       {/* Content */}
