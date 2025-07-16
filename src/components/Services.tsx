4aacec2f92947d4d0eafb81a0075d603600232ad
GGabilondo <ggabilondo.1@gmail.com>
2025-07-16 08:17:51 +0200

    Update TCG DEEP CLEAN image to use TCG_DEEP_CLEAN.jpg

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -35,7 +35,7 @@ const Services = () => {
     {
       name: 'TCG DEEP CLEAN',
       price: '£150 - £175',
-      image: '/images/interior_cleaning_audi-tiruy55r.png',
+      image: '/images/TCG_DEEP_CLEAN.jpg',
       exteriorFeatures: [
         'Engine bay cleaned',
         'Wheels, tyres & wheel wells detailed',
